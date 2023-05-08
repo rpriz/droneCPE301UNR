cbf7c4ed12af60e22b2df43d6d330c3f4d535d67
rpriz <130603050+rpriz@users.noreply.github.com>
2023-05-07 18:08:53 -0700

    digitalRite doesn't work.gyro input corrected.serial comm not working
    change all use of digitalRite to direct port manipulation. Coded on/off[red/greem] led for ADC and flight controller. Coded ADC to pin 1[analog]. partial fix of serial communication function.

--- drone_controller_V2_APC220_Serial.ino
+++ drone_controller_V2_APC220_Serial.ino
@@ -16,7 +16,7 @@
 
 //masks
  //Reciever channels
-#define CH1Rec 0x10  //pi  n 10
+#define CH1Rec 0x10  //pin 10
 #define CH2Rec 0x20  //pin 11
 #define CH3Rec 0x40  //pin 12
 #define CH4Rec 0x80   //pin 13  
@@ -54,6 +54,8 @@ unsigned long countmicros;
   volatile unsigned char *myPCMSK0 = (unsigned char *) 0x6B;  //set register 0 to 1 to enable interupts
   volatile unsigned char *myPCICR = (unsigned char *) 0x68;      //set register 7,6,5,4 to 1 to enable interupts pins 13,12,12,10 respectively
 
+  //gyro calibration variable
+  unsigned long gyroTimeSet;
   //variables for wave calculation 
   unsigned long wave_CH_1, wave_CH_2 , wave_CH_3, wave_CH_4, wave_CH_6;
   int state_CH_1, state_CH_2, state_CH_3, state_CH_4,state_CH_6;
@@ -70,12 +72,17 @@ unsigned long countmicros;
 
 //Port E pins 2 / 3
 //port GPIO
-volatile unsigned char* port_e = (unsigned char*)0x2E; //pins 2/3 use registers 4/5 respectively
+
 volatile unsigned char* ddr_e = (unsigned char*)0x2D; //pins 2/3 use registers 4/5 respectively
 volatile unsigned char* pin_e = (unsigned char*)0x2C; //pins 2/3 use registers 4/5 respectively
 volatile unsigned char* port_h = (unsigned char*)0x102;  // pins 6/7 use  registers 3/4  respectively
 volatile unsigned char* pin_h = (unsigned char*)0x100;
 volatile unsigned char* pin_b = (unsigned char*)0x23;
+volatile unsigned char* port_a = (unsigned char*)0x22;
+volatile unsigned char* port_b = (unsigned char*)0x25;
+volatile unsigned char* port_c = (unsigned char*)0x28;
+volatile unsigned char* port_e = (unsigned char*)0x2E; //pins 2/3 use registers 4/5 respectively
+
 //Port H
 
 //input pins
@@ -86,14 +93,14 @@ volatile unsigned char* pin_b = (unsigned char*)0x23;
 
  char swchpin = '1'; //channel 6 from reciever
 //output pins
-  char moPin1 = '5'; //Motor 1 Front Left
-  char moPin2 = '2'; //Motor 2 Front Right
-  char moPin3 = '3'; //Motor 3 Back Left
-  char moPin4 = '4'; //Motor 4 Back Right
-  const int REDLed = 39;
-  const int GREENLed = 40;
+  char moPin1 = '5'; //Motor 1 Front Left channel 4
+  char moPin2 = '2'; //Motor 2 Front Right channel 3
+  char moPin3 = '3'; //Motor 3 Back Left channel 2
+  char moPin4 = '4'; //Motor 4 Back Right channel 1
+
   //int variables 
-  int timer = 0;
+ // int timer = 0;
+  int timer3 = 0;
 //ESC defined
 Servo ESC_FL;     // create servo object to control the ESC Front Left Motor 1 pin 5
 Servo ESC_FR;     // create servo object to control the ESC Fromnt Right Motor 2 pin 2
@@ -102,6 +109,8 @@ Servo ESC_BR;     // create servo object to control the ESC Back Right  Motor 4 
 
 #define RDA 0x80
 #define TBE 0x20
+
+//set up registers for serial communication 
 volatile unsigned char *myUCSR1A = (unsigned char *)0xC8;
 volatile unsigned char *myUCSR1B = (unsigned char *)0xC9;
 volatile unsigned char *myUCSR1C = (unsigned char *)0xCA;
@@ -112,22 +121,37 @@ volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
 volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
 volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
 volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
-//volatile unsigned char* Port_L = (unsigned char*) 0x98;
 
+
+//GPIO variables
+
+int fON = 28;
+int fOFF = 36;
+int adcPin = 107;
+int adcRed =51;
+int adcGreen = 53;
+///////////////////////////PROGRAM SETUP/////////////////////////////////////////////////////////////
 void setup() {
   //start serial for testing
   Serial. begin(9600);
+  //start hardware serial on pins 18/19
   U1init(9600);
   // setup the ADC
   adc_init();
-  // setup the pins
- // *Port_L |= 0b00000100; // set pin 42 as output
- // *Port_L |= 0b00000010; // set pin 41 as output
-  pinner(REDLed,'o');
-  pinner(GREENLed,'o');
+
+
   //start gyro
    Wire.begin();
   //configure pins for input or output
+  //GPIO red/green lights battery ADC and Flight COntroller on/off
+    // setup the pins for ADC battery voltage lights 51 RED 53 GREEN 
+  pinner(adcRed,'o');
+  pinner(adcGreen,'o');
+    //pins for flight controller on/off 28(PA6) GREEN 36(PC1) RED
+   pinner(fON, 'o');
+   pinner(fOFF, 'o');
+   //analog pin 1 input for ADC
+   pinner(adcPin, 'i');
   //ESC outputs
   pinner(moPin1,'o'); //Motor 1 Front Left
   pinner(moPin2,'o');//Motor 2 Front Riger
@@ -163,9 +187,13 @@ byte status = mpu.begin();
    Serial.print(F("MPU6050 status: "));
    Serial.println(status);
    while (status != 0) { } // stop everything if could not connect to MPU6050
- Serial.println(F("Calculating offsets, do not move MPU6050"));
-   delay(1000);
+   gyroTimeSet = gTime();
+   Serial.println(F("Calculating offsets, do not move MPU6050"));
+   //while(gyroTimeSet < gTime() - 1000000){ 
+
+  
    mpu.calcOffsets(); // gyro and accelero
+//}
    Serial.println("Done!\n");
 //esc setup
 
@@ -175,60 +203,31 @@ ESC_BL.attach(3,1000,2000);
 ESC_BR.attach(4,1000,2000); 
 }
 
+
+///////////////////////////////MAIN LOOP/////////////////////////////////////////////
+
+
 void loop() {
   //on/off button 
-  gyroMPU();
-  if(wave_CH_6 > 1500)
-  {
-  int i = 0;
-  
-  if(i = 100000)
-  {
 
-i=0;
+  //if switch is turned off stop program
+  if(wave_CH_6 <= 1500){
+
+ 
+    //turn off green light turn on red
+  *port_a &= ~PX6;
+   *port_c |= PX1;
+
+ 
   }
-else{
-i++;
-}
-
-//Serial.println(*pin_b & CH2Rec);
-if(wave_CH_1 >0)
-{
-Serial.println("1");
-Serial.println(wave_CH_1);
-
-
-}
-if(wave_CH_2 >0)
-{
-Serial.println("2");
-Serial.println(wave_CH_2);
-
-
-}
-if(wave_CH_3 >0)
-{
-Serial.println("3");
-Serial.println(wave_CH_3);
-
-
-}
-if(wave_CH_4 >0)
-{
-Serial.println("4");
-Serial.println(wave_CH_4);
-
-
-}
-if(wave_CH_6 >0)
-{
-
-Serial.println("6");
-Serial.println(wave_CH_6);
-
-}
-
-//Serial.println(*pin_b & CH4Rec);
+  //if switch is turned on run program
+  else if(wave_CH_6 > 1500)
+  {
+   //turn on green light turn off red
+  *port_c &= ~PX1;
+   *port_a |= PX6;
+     gyroMPU();
+ 
 
  //end button if statement
   }
@@ -244,18 +243,19 @@ ESC_BR.writeMicroseconds(wave_CH_3);
 interrupts();
 
   // get the reading from the ADC
-  unsigned int adc_reading = adc_read(0);
-  // If the ADC reading is less than 950, turn on pin 42 and turn off pin 41
-  if(adc_reading < 950)
+  unsigned int adc_reading = adc_read(7);
+  // If the ADC reading is less than 700, turn off green led turn on red
+  if(adc_reading < 700)
   {
-    digitalRite(REDLed,1);
-    digitalRite(GREENLed,0);
+    //28(PA6) GREEN 36(PC1) RED
+  *port_b &= ~PX0;
+   *port_b |= PX2;
   }
-  // If the ADC reading is greater than or equal to 950, turn on pin 41 and turn off pin 42
+  // If the ADC reading is greater than or equal to 700, turn on green led turn off red
   else
   {
-    digitalRite(REDLed,0);
-    digitalRite(GREENLed,1);
+  *port_b &= ~PX2;
+   *port_b |= PX0;
   }
 
 } //end main loop
@@ -277,8 +277,11 @@ unsigned long gTime()
 void gyroMPU()
 {
    mpu.update();
- if ((millis() - timer) > 10) { // print data every 10ms
+ if ((millis() - timer3) > 1000) { // print data every 10ms
+    
+ /*
      U1putchar("X : ");
+     
      U1putchar(mpu.getAngleX());
      U1putchar("\tY : ");
      U1putchar(mpu.getAngleY());
@@ -286,9 +289,32 @@ void gyroMPU()
      U1putchar(mpu.getAngleZ());
      U1putchar('\n');
      timer = millis();
+     Serial1.write('hi')
+     */
+     float xdir = mpu.getAngleX();
+         Serial.write("x: ");
+       Serial.print(mpu.getAngleX());
+       Serial.write('\n');
+     Serial.write("y: ");
+       Serial.print(mpu.getAngleY());
+       Serial.write('\n');
+        Serial.write("z: ");
+       Serial.print(mpu.getAngleZ());
+       Serial.write('\n');
+
+  int num = -1234;  // Signed integer to send
+  char str[10];  // Buffer to hold converted string
+  sprintf(str, "%d", num);  // Convert integer to string
+  
+  int len = strlen(str);  // Get length of string
+  
+  for (int i = 0; i < len; i++) {
+    while (!(UCSR1A & (1 << UDRE1)));  // Wait for empty transmit buffer
+    UDR1 = str[i];  // Send character
+       
    }
 
-
+ }
 
 }
 void recieverInput()
